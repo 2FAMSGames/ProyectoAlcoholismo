cdd7c9265f983989f9ae5d9d306ae93f69c62476
Miguel Jimenez <migueljg1997@gmail.com>
2023-03-03 19:03:11 +0100

    Added drag and drop behaviour

--- ProyectoAlcoholismo/Assets/Minigames/Lanzapato/Scripts/SlingshotController.cs
+++ ProyectoAlcoholismo/Assets/Minigames/Lanzapato/Scripts/SlingshotController.cs
@@ -7,31 +7,35 @@ public class SlingshotController : MonoBehaviour
     [SerializeField]
     GameObject projectionPlaneObject;
 
+    [SerializeField]
+    GameObject projectilePrefab;
+
+    [SerializeField]
+    [Tooltip("Position where projectile will be instantiated and collider used for click detection")]
+    GameObject projectilePlacer;
+
+    SphereCollider projectilePlacerCollider;
+
     Plane projectionPlane;
+    bool clicked = false;
+    GameObject projectile;
+    Vector3 mousePos;
 
     // Start is called before the first frame update
     void Start()
     {
         projectionPlane = new Plane(projectionPlaneObject.transform.up, projectionPlaneObject.transform.position);
+        projectilePlacerCollider = projectilePlacer.GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        /*if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            touchPosWords = Input.GetTouch(0).position;
-        }*/
+        CheckOnClickEnter();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 clickPos = GetClickPositionOnPlane().GetValueOrDefault();
+        CheckOnClic();
 
-            GameObject clickSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            clickSphere.transform.position = clickPos;
-
-        }
+        CheckOnClickExit();
     }
 
     private Vector3? GetClickPositionOnPlane()
@@ -47,5 +51,46 @@ public class SlingshotController : MonoBehaviour
         return null;
     }
 
+    private void CheckOnClickEnter()
+    {
+        if (clicked)
+        {
+            mousePos = GetClickPositionOnPlane().GetValueOrDefault();
+            projectile.transform.position = mousePos;
+        }
+    }
 
+    private void CheckOnClickExit()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(projectile);
+            clicked = false;
+        }
+    }
+
+    private void CheckOnClic()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = GetClickPositionOnPlane().GetValueOrDefault();
+
+            //Only detects if mouse position is inside placer bounding sphere
+            if (projectilePlacerCollider.bounds.Contains(mousePos))
+            {
+                projectile = Instantiate(projectilePrefab, mousePos, Quaternion.identity, gameObject.transform);
+                clicked = true;
+            }
+        }
+    }
 }
+
+
+
+
+/*
+if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+{
+    touchPosWords = Input.GetTouch(0).position;
+}
+*/
